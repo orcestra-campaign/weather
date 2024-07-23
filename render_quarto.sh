6b7690e0f698fff438c2d527af9c236bcb2a4843
Luca Schmidt <72014857+Lucalino@users.noreply.github.com>
2024-07-23 13:31:43 +0200

    Quarto setup (#10)
    * Adapt build scripts for new folder structure
    
    * Define python environment, add environment.yaml
    
    * Remove outdated filename
    
    * Ignore quarto build artefacts
    
    Ignore both the jupyter cache as well as the created IPython notebook

--- render_quarto.sh
+++ render_quarto.sh
@@ -1,10 +1,10 @@
 # render the final quarto file
+set -o xtrace
 
-briefing_date=${1}
+quarto_file=${1}
 quarto_command=${2:-preview}
 quarto_profile=${3:-slides}
 
-quarto_file=./briefings/${briefing_date}/main.qmd
 if [ ${quarto_command} = 'preview' ]; then
     quarto preview ${quarto_file} --no-navigate --profile ${quarto_profile}
 elif [ ${quarto_command} = 'render' ]; then

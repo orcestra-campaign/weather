ce7daa945d861d0a4a0d926364fefaceb1fae28a
Sebastian Ortega <sebastian.ortega@mpimet.mpg.de>
2024-07-23 16:12:28 +0200

    changing render_quatro back

--- render_quarto.sh
+++ render_quarto.sh
@@ -1,10 +1,10 @@
 # render the final quarto file
-set -o xtrace
 
-quarto_file=${1}
+briefing_date=${1}
 quarto_command=${2:-preview}
 quarto_profile=${3:-slides}
 
+quarto_file=./briefings/${briefing_date}/main.qmd
 if [ ${quarto_command} = 'preview' ]; then
     quarto preview ${quarto_file} --no-navigate --profile ${quarto_profile}
 elif [ ${quarto_command} = 'render' ]; then

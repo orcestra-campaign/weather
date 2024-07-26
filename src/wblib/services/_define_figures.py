8d379730f9c8ea928b6af9d644d22b7a17dde7ef
Sebastian Ortega <sebastian.ortega@mpimet.mpg.de>
2024-07-26 10:26:58 +0200

    Adding NHC seven days outlook to report

--- src/wblib/services/_define_figures.py
+++ src/wblib/services/_define_figures.py
@@ -2,12 +2,13 @@
 
 from wblib.figures.external.goes import current_satellite_image_ir
 from wblib.figures.external.goes import current_satellite_image_vis
-from wblib.figures.external.noaa import nhc_surface_analysis_atlantic
+from wblib.figures.external.noaa import nhc_seven_days_outlook, nhc_surface_analysis_atlantic
 from wblib.figures.internal.icwv import iwv_itcz_edges
 
 
 EXTERNAL_PLOTS = {
     "nhc_surface_analysis_atlantic": nhc_surface_analysis_atlantic,
+    "nhc_seven_days_outlook": nhc_seven_days_outlook,
     "current_satellite_image_vis": current_satellite_image_vis,
     "current_satellite_image_ir": current_satellite_image_ir,
 }

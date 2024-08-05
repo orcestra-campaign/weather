050778c8de5c63154505e34d9b9426c6030adfe5
Henning Franke <henning.franke.182@gmail.com>
2024-08-05 15:18:37 +0200

    Include precip plots in the briefing.

--- src/wblib/services/_define_figures.py
+++ src/wblib/services/_define_figures.py
@@ -2,8 +2,10 @@
 
 from wblib.figures.external.goes import current_satellite_image_ir
 from wblib.figures.external.goes import current_satellite_image_vis
-from wblib.figures.external.noaa import nhc_seven_days_outlook, nhc_surface_analysis_atlantic
+from wblib.figures.external.noaa import nhc_seven_days_outlook
+from wblib.figures.external.noaa import nhc_surface_analysis_atlantic
 from wblib.figures.internal.icwv import iwv_itcz_edges
+from wblib.figures.internal.precip import precip
 
 
 EXTERNAL_PLOTS = {
@@ -14,6 +16,7 @@ EXTERNAL_PLOTS = {
 }
 
 INTERNAL_PLOTS = {
-    "iwv_itcz_edges": iwv_itcz_edges
+    "iwv_itcz_edges": iwv_itcz_edges,
+    "precip": precip,
 }
 INTERNAL_PLOTS_LEADTIMES = ["003h", "012h", "036h", "060h", "084h", "108h"]

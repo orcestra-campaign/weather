fef67e736b59be59bfe8b8c3092a9ee9c2d0c0f4
Divya Sri Praturi <dspraturi@gmail.com>
2024-08-05 14:32:45 +0200

    10m surface wind speed & quivers: added to wbcli workflow

--- src/wblib/figures/internal/sfc_wind_quivers.py
+++ src/wblib/figures/internal/sfc_wind_quivers.py
@@ -29,20 +29,30 @@ def surface_wind_quivers(briefing_time: pd.Timestamp, lead_hours: str) -> Figure
     lead_delta = pd.Timedelta(hours=int(lead_hours[:-1]))
     issued_time = get_latest_forecast_issue_time(briefing_time)
     refdate = issued_time.strftime("%Y-%m-%d")
+    print(refdate)
+    # issued_time = '2024-08-04'
+    # refdate = '2024-08-04'
     cat = intake.open_catalog(CATALOG_URL)
     ds = cat['HIFS'](refdate=refdate).to_dask().pipe(egh.attach_coords)
-    # windspeed_100m = np.sqrt(ds['100u']**2 + ds['100v']**2)
-
-    windspeed_10m = np.sqrt(ds['10u']**2 + ds['10v']**2)
     lon_min, lon_max, lat_min, lat_max = FIGURE_BOUNDARIES
+   
+    # windspeed_100m = np.sqrt(ds['100u']**2 + ds['100v']**2)
+    windspeed_10m = np.sqrt(ds['10u']**2 + ds['10v']**2)
+    lon1 = np.linspace(lon_min, lon_max, MESH_GRID_SIZE)
+    lat1 = np.linspace(lat_min, lat_max, MESH_GRID_SIZE)
+    pix = xr.DataArray(
+        hp.ang2pix(ds.crs.healpix_nside, *np.meshgrid(lon1, lat1), nest=True, lonlat=True),
+        coords=(("lat1", lat1), ("lon1", lon1)),
+    )
     valid_time = briefing_time + lead_delta
     valid_time = valid_time.tz_localize(None)
 
-    # plotting function
+    # plotting
     sns.set_context('talk')
-    fig, ax = plt.subplots(figsize=FIGURE_SIZE, subplot_kw={"projection": ccrs.PlateCarree()})
+    fig, ax = plt.subplots(figsize=FIGURE_SIZE, subplot_kw={"projection": ccrs.PlateCarree()}, facecolor='white')
     ax.set_extent([lon_min, lon_max, lat_min, lat_max])
     ax.coastlines(lw=0.8)
+    # filled contours
     im1 = egh.healpix_show(
         windspeed_10m.sel(time=valid_time),
         method="linear",
@@ -50,72 +60,50 @@ def surface_wind_quivers(briefing_time: pd.Timestamp, lead_hours: str) -> Figure
         vmin=SPEED_MIN,
         vmax=SPEED_MAX,
     )   
-    fig.colorbar(im1, shrink = 0.8)
-    lon1 = np.linspace(lon_min, lon_max, MESH_GRID_SIZE)
-    lat1 = np.linspace(lat_min, lat_max, MESH_GRID_SIZE)
-
-    pix = xr.DataArray(
-        hp.ang2pix(ds.crs.healpix_nside, *np.meshgrid(lon1, lat1), nest=True, lonlat=True),
-        coords=(("lat1", lat1), ("lon1", lon1)),
-    )
+    # quiver plot
     Q0 = ax.quiver(lon1[::QUIVER_SKIP], lat1[::QUIVER_SKIP], ds['10u'].sel(time=valid_time).isel(cell = pix)[::QUIVER_SKIP, ::QUIVER_SKIP]
                    , ds['10v'].sel(time = valid_time).isel(cell = pix)[::QUIVER_SKIP, ::QUIVER_SKIP]
                    , color = 'black', pivot = 'middle', scale_units = 'inches', width = 0.003, scale = 20) 
-
     ax.quiverkey(Q0, 0.95, 1.02, 10, r'$10 \frac{m}{s}$', labelpos='E', coordinates='axes', animated=True)
-
+    # 3 m/s wind speed contour
     im2 = egh.healpix_contour(windspeed_10m.sel(time=valid_time), ax = ax, 
             levels = [SPEED_THRESHOLD], colors = 'r'
             )
+    # axes formatting
+    fig.colorbar(im1, label = '10m wind speed / m s$^{-1}$', shrink = 0.8)
+    plt.clabel(im2, inline=True, fontsize=12, colors='r', fmt="%d")
+    _speed_format_axes(briefing_time, issued_time, lead_delta, ax)
+    matplotlib.rc_file_defaults()
+    return fig
     
-    def _speed_format_axes(briefing_time, issued_time, lead_delta, ax):
-        
+def _speed_format_axes(briefing_time, issued_time, lead_delta, ax):
+    lon_min, lon_max, lat_min, lat_max = FIGURE_BOUNDARIES
+    valid_time = briefing_time + lead_delta
+    title_str = (
+            f"Valid time: {valid_time.strftime('%Y-%m-%d %H:%M')} \n"
+            f"Lead hours: {int(lead_delta.total_seconds() / 3600):03d}"
+                     )
+    ax.set_title(title_str)
+    ax.coastlines(lw=1.0, color="k")
+    ax.set_xticks(np.round(np.linspace(lon_min+5, lon_max-5, 6),0), crs=ccrs.PlateCarree())
+    ax.set_yticks(np.round(np.linspace(lat_min, lat_max, 5),0), crs=ccrs.PlateCarree()) 
+    ax.set_ylabel("Latitude \N{DEGREE SIGN}N")
+    ax.set_xlabel("Longitude \N{DEGREE SIGN}E")
+    ax.set_xlim([lon_min, lon_max])
+    ax.set_ylim([lat_min, lat_max])
+    annotation = f"Latest ECMWF IFS forecast initialization: {issued_time.strftime('%Y-%m-%d %H:%M %Z')}"
+    ax.annotate(annotation,
+                (-32.25, -9),
+                fontsize=8,
+                bbox = dict(facecolor='white',
+                            edgecolor='none',
+                            alpha=1))
+    
+# # if name == "main":
+# briefing_time = pd.Timestamp(2024, 8, 4, tz="UTC")
+# lead_hours = "048H"
+# fig = surface_wind_quivers(briefing_time, lead_hours)
+# # fig.show()
+# fig.savefig("test.png")
 
-# date = "2024-08-04"
-# plot_time = "2024-08-11T12:00:00.000000000"
-
-# cat = intake.open_catalog("https://tcodata.mpimet.mpg.de/internal.yaml")
-# ds = cat['HIFS'](refdate=date, reftime='12').to_dask().pipe(egh.attach_coords)
-# windspeed_100m = np.sqrt(ds['100u']**2 + ds['100v']**2)
-# windspeed_10m = np.sqrt(ds['10u']**2 + ds['10v']**2)
-
-# lon_min, lon_max, lat_min, lat_max = FIGURE_BOUNDARIES
-
-# fig, ax = plt.subplots(figsize=FIGURE_SIZE, subplot_kw={"projection": ccrs.PlateCarree()})
-# ax.set_extent([lon_min, lon_max, lat_min, lat_max])
-# ax.coastlines(lw=0.8)
-# im1 = egh.healpix_show(
-#     windspeed_10m.sel(time=plot_time),
-#     method="linear",
-#     cmap=SPEED_COLORMAP,
-#     vmin=SPEED_MIN,
-#     vmax=SPEED_MAX,
-# )
-
-
-
-
-# lon1 = np.linspace(lon_min, lon_max, mesh_grid_size)
-# lat1 = np.linspace(lat_min, lat_max, mesh_grid_size)
-
-# pix = xr.DataArray(
-#     hp.ang2pix(ds.crs.healpix_nside, *np.meshgrid(lon1, lat1), nest=True, lonlat=True),
-#     coords=(("lat1", lat1), ("lon1", lon1)),
-# )
-
-# skip = 4
-# Q0 = ax.quiver(lon1[::skip], lat1[::skip], ds['100u'].sel(time=plot_time).isel(cell = pix)[::skip, ::skip], ds['100v'].sel(time = plot_time).isel(cell = pix)[::skip, ::skip], 
-#                 color = 'black', pivot = 'middle', scale_units = 'inches', width = 0.003, scale = 20) 
-
-# ax.quiverkey(Q0, 0.95, 1.02, 10, r'$10 \frac{m}{s}$', labelpos='E', coordinates='axes', animated=True)
-
-
-
-plt.clabel(im2, inline=True, fontsize=12, colors='r', fmt="%d")
-ax.set_xticks(np.round(np.linspace(lon_min+5, lon_max-5, 6),0), crs=ccrs.PlateCarree())
-ax.set_yticks(np.round(np.linspace(lat_min, lat_max, 5),0), crs=ccrs.PlateCarree()) 
-ax.set_ylabel('Latitude/ \N{DEGREE SIGN}N')
-ax.set_xlabel('Longitude/ \N{DEGREE SIGN}E')
-
-plt.show()
 
--- src/wblib/services/_define_figures.py
+++ src/wblib/services/_define_figures.py
@@ -6,6 +6,7 @@ from wblib.figures.external.noaa import nhc_hovmoller
 from wblib.figures.external.noaa import nhc_seven_days_outlook
 from wblib.figures.external.noaa import nhc_surface_analysis_atlantic
 from wblib.figures.internal.icwv import iwv_itcz_edges
+from wblib.figures.internal.sfc_wind_quivers import surface_wind_quivers
 
 
 EXTERNAL_PLOTS = {
@@ -17,6 +18,7 @@ EXTERNAL_PLOTS = {
 }
 
 INTERNAL_PLOTS = {
-    "iwv_itcz_edges": iwv_itcz_edges
+    "iwv_itcz_edges": iwv_itcz_edges,
+    "surface_wind_quivers": surface_wind_quivers
 }
 INTERNAL_PLOTS_LEADTIMES = ["003h", "012h", "036h", "060h", "084h", "108h"]

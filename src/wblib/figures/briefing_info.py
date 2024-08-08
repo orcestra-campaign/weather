9e7ebd0e4f50546ea580d2fcc92b4dec99b8c1c2
Sebastian Ortega <sebastian.ortega@mpimet.mpg.de>
2024-08-08 11:55:22 +0200

    removing unused functions from briefing_info.py

--- src/wblib/figures/briefing_info.py
+++ src/wblib/figures/briefing_info.py
@@ -1,11 +1,6 @@
 """Functions to work with IFS forecasts in healpix format."""
 import pandas as pd
 
-
-FORECAST_PUBLISH_LAG = "8h"
-FORECAST_PUBLISH_FREQ = "12h"
-
-
 def get_valid_time(
     briefing_time: pd.Timestamp, briefing_lead_hours: str
 ) -> pd.Timestamp:
@@ -13,25 +8,3 @@ def get_valid_time(
     briefing_delta = pd.Timedelta(hours=int(briefing_lead_hours[:-1]))
     valid_time = briefing_time + briefing_delta
     return valid_time
-
-
-def get_latest_forecast_issue_time(briefing_time: pd.Timestamp):
-    current_time = pd.Timestamp.now("UTC")
-    publish_lag = pd.Timedelta(FORECAST_PUBLISH_LAG)
-    publish_freq = pd.Timedelta(FORECAST_PUBLISH_FREQ)
-    if current_time >= briefing_time + publish_lag:
-        issued_time = briefing_time
-    if current_time < briefing_time + publish_lag:
-        issued_time = current_time.floor(FORECAST_PUBLISH_FREQ) - publish_freq
-    return issued_time
-
-
-def get_dates_of_previous_initializations(
-    issue_time: pd.Timestamp,
-    number: int=5,
-) -> list[pd.Timestamp]:
-    day = pd.Timedelta("1D")
-    dates = [(issue_time.floor("1D") - i * day) for i in
-             range(0, number)]
-    dates.reverse()
-    return dates

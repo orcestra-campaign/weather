9c0c91b2934b6fc63b8d3c0a7b512e5b338e36c2
Sebastian Ortega <sebastian.ortega@mpimet.mpg.de>
2024-08-08 11:34:26 +0200

    precip refactor

--- src/wblib/figures/internal/precip.py
+++ src/wblib/figures/internal/precip.py
@@ -15,20 +15,16 @@ from matplotlib.colors import BoundaryNorm
 import seaborn as sns
 import cmocean as cmo
 
-#import orcestra.sat
-#from wblib.figures.internal._general_plotting_functions import plot_sattrack
+from wblib.figures.hifs import HifsForecasts
+from wblib.figures.briefing_info import get_valid_time
 
-from wblib.figures.briefing_info import get_latest_forecast_issue_time
-from wblib.figures.briefing_info import get_dates_of_previous_initializations
-
-CATALOG_URL = "https://tcodata.mpimet.mpg.de/internal.yaml"
 TP_THRESHOLD = 50  # mm
-TCWV_THRESHOLD = 48 # mm
+TCWV_THRESHOLD = 48  # mm
 TP_STEPS = [0, 5, 25, 50, 75, 100]
 TP_COLORMAP = cmo.cm.rain
 DATA_CATALOG_VARIABLE = ["tp", "tcwv"]
 FIGURE_SIZE = (15, 8)
-DOMAIN = 'Sal'
+DOMAIN = "Sal"
 REFDATE_COLORBAR_TP = [
     "#ff7e26",
     "#ff580f",
@@ -38,132 +34,107 @@ REFDATE_COLORBAR_TCWV = [
     "royalblue",
 ]
 REFDATE_LINEWIDTH = [1.0, 1.4]
+FIGURE_BOUNDARIES = (-70, 10, -10, 30)
 
-def precip(briefing_time: pd.Timestamp, lead_hours: str) -> Figure:
+
+def precip(
+    briefing_time: pd.Timestamp,
+    lead_hours: str,
+    current_time: pd.Timestamp,
+    hifs: HifsForecasts,
+) -> Figure:
     # retrieve the forecast data
-    lead_delta = pd.Timedelta(hours=int(lead_hours[:-1]))
-    issued_time = get_latest_forecast_issue_time(briefing_time)
-    issued_times = get_dates_of_previous_initializations(issued_time, number=2)
-    datarrays = _get_forecast_datarrays_dict(issued_times)
-    precip = {init_time: datarrays[init_time]['tp'] for init_time in
-              issued_times}
-    tcwv = {init_time: datarrays[init_time]['tcwv'] for init_time in
-            issued_times}
-    for init_time in issued_times:
-        precip[init_time] = precip[init_time].differentiate(
-            'time', datetime_unit='D') * 1000
-
+    issue_time, precip = hifs.get_forecast(
+        DATA_CATALOG_VARIABLE[0],
+        briefing_time,
+        lead_hours,
+        current_time,
+        differentiate=True,
+        differentiate_unit="D",
+    )
+    precip = 1000 * precip
+    precip_forecasts = hifs.get_previous_forecasts(
+        DATA_CATALOG_VARIABLE[0],
+        briefing_time,
+        lead_hours,
+        current_time,
+        number=2,
+        differentiate=True,
+        differentiate_unit="D",
+    )
+    precip_forecasts = (
+        (issue_time, 1000 * precip) for issue_time, precip in precip_forecasts
+    )
+    tcwv_forecasts = hifs.get_previous_forecasts(
+        DATA_CATALOG_VARIABLE[1],
+        briefing_time,
+        lead_hours,
+        current_time,
+        number=2,
+    )
     # plotting
-    sns.set_context('talk')
+    sns.set_context("talk")
     fig, ax = plt.subplots(
         figsize=FIGURE_SIZE, subplot_kw={"projection": ccrs.PlateCarree()}
     )
-    _draw_tp_contours_for_previous_forecasts(
-        precip, briefing_time, lead_delta, issued_times, ax
-    )
-    _draw_tcwv_contours_for_previous_forecasts(
-        tcwv, briefing_time, lead_delta, issued_times, ax
-    )
-    im = _draw_current_forecast(
-        precip, briefing_time, lead_delta, issued_times, ax
-    )
-    #plot_sattrack(valid_time, ax)
-    _format_axes(briefing_time, lead_delta, ax)
-    _add_legend(issued_times, loc='lower right')
-    fig.colorbar(im, label="mean precip. rate / mm day$^{-1}$",
-                 shrink=0.7)
+    _format_axes(briefing_time, lead_hours, issue_time, ax)
+    _draw_tp_contours_for_previous_forecasts(precip_forecasts, ax)
+    _draw_tcwv_contours_for_previous_forecasts(tcwv_forecasts, ax)
+    _draw_current_forecast(precip, fig, ax)
     matplotlib.rc_file_defaults()
     return fig
 
-def _select_latlonbox(domain: str) -> tuple:
-    if domain == 'full_Atlantic':
-        return (-70, 10, -10, 30)
-    elif domain == 'Sal':
-        return (-50, 0, -5, 20)
-    elif domain == 'Barbados':
-        return (-70, 10, -10, 30)
-    else:
-        raise ValueError ('Please provide a valid domain! Valid domains are:' +
-                          ' "full_Atlantic", "Sal", and "Barbados".')
 
-
-def _get_forecast_datarrays_dict(init_times):
-    catalog = intake.open_catalog(CATALOG_URL)
-    datarrays = dict()
-    for init_time in init_times:
-        refdate = init_time.strftime("%Y-%m-%d")
-        dataset = (
-            catalog.HIFS(refdate=refdate).to_dask().pipe(egh.attach_coords)
-        )
-        datarrays[init_time] = dataset[DATA_CATALOG_VARIABLE]
-    return datarrays
-
-def _draw_tp_contours_for_previous_forecasts(
-    datarrays, briefing_time, lead_delta, init_times, ax
-):
-    lon_min, lon_max, lat_min, lat_max = _select_latlonbox(DOMAIN)
-    ax.set_extent(
-        [lon_min, lon_max, lat_min, lat_max]
-    )  # need this line here to get the contours and lines on the plot
-    for i, init_time in enumerate(init_times):
+def _draw_tp_contours_for_previous_forecasts(precip_forecasts, ax):
+    for i, (_, precip) in enumerate(precip_forecasts):
         color = REFDATE_COLORBAR_TP[i]
         linewidth = REFDATE_LINEWIDTH[i]
-        valid_time = briefing_time + lead_delta
-        valid_time = valid_time.tz_localize(None)
-        field = datarrays[init_time].sel(time=valid_time)
-        im = egh.healpix_contour(
-            field,
+        egh.healpix_contour(
+            precip,
             ax=ax,
             levels=[TP_THRESHOLD],
             colors=color,
             linewidths=linewidth,
         )
 
-def _draw_tcwv_contours_for_previous_forecasts(
-    datarrays, briefing_time, lead_delta, init_times, ax
-):
-    lon_min, lon_max, lat_min, lat_max = _select_latlonbox(DOMAIN)
-    ax.set_extent(
-        [lon_min, lon_max, lat_min, lat_max]
-    )  # need this line here to get the contours and lines on the plot
-    for i, init_time in enumerate(init_times):
+
+def _draw_tcwv_contours_for_previous_forecasts(tcwv_forecasts, ax):
+    issued_times = []
+    for i, (issued_time_, tcwv_forecasts) in enumerate(tcwv_forecasts):
         color = REFDATE_COLORBAR_TCWV[i]
         linewidth = REFDATE_LINEWIDTH[i]
-        valid_time = briefing_time + lead_delta
-        valid_time = valid_time.tz_localize(None)
-        field = datarrays[init_time].sel(time=valid_time)
         im = egh.healpix_contour(
-            field,
+            tcwv_forecasts,
             ax=ax,
             levels=[TCWV_THRESHOLD],
-            linestyles = '--',
+            linestyles="--",
             colors=color,
             linewidths=linewidth,
         )
         ax.clabel(im, inline=True, fontsize=10)
+        issued_times.append(issued_time_)
+    _add_legend(issued_times, loc="lower left")
 
-def _draw_current_forecast(
-    datarrays, briefing_time, lead_delta, init_times, ax
-):
-    valid_time = briefing_time + lead_delta
-    valid_time = valid_time.tz_localize(None)
-    field = datarrays[init_times[-1]].sel(time=valid_time)
+
+def _draw_current_forecast(precip, fig, ax):
     im = egh.healpix_show(
-        field,
+        precip,
         ax=ax,
         method="linear",
-        norm = BoundaryNorm(TP_STEPS, ncolors=TP_COLORMAP.N, clip=True),
+        norm=BoundaryNorm(TP_STEPS, ncolors=TP_COLORMAP.N, clip=True),
         cmap=TP_COLORMAP,
     )
-    return im
+    fig.colorbar(im, label="mean precip. rate / mm day$^{-1}$", shrink=0.7)
 
-def _format_axes(briefing_time, lead_delta, ax):
-    lon_min, lon_max, lat_min, lat_max = _select_latlonbox(DOMAIN)
-    valid_time = briefing_time + lead_delta
+
+def _format_axes(briefing_time, lead_hours, issue_time, ax):
+    lon_min, lon_max, lat_min, lat_max = FIGURE_BOUNDARIES
+    valid_time = get_valid_time(briefing_time, lead_hours)
+    ax.set_extent([lon_min, lon_max, lat_min, lat_max])
     title_str = (
         f"Valid time: {valid_time.strftime('%Y-%m-%d %H:%M')}UTC \n"
-        f"Lead hours: {int(lead_delta.total_seconds() / 3600):03d}"
-                 )
+        f"Lead hours: {lead_hours}"
+    )
     ax.set_title(title_str)
     ax.coastlines(lw=1.0, color="k")
     ax.set_xticks(np.round(np.linspace(-70, 10, 9), 0), crs=ccrs.PlateCarree())
@@ -172,18 +143,40 @@ def _format_axes(briefing_time, lead_delta, ax):
     ax.set_xlabel("Longitude / \N{DEGREE SIGN}E")
     ax.set_xlim([lon_min, lon_max])
     ax.set_ylim([lat_min, lat_max])
+    annotation = f"Latest ECMWF IFS forecast initialization: {issue_time.strftime('%Y-%m-%d %H:%M %Z')}"
+    ax.annotate(
+        annotation,
+        (-21.25, -9),
+        fontsize=8,
+        bbox=dict(facecolor="white", edgecolor="none", alpha=1),
+    )
+
 
 def _add_legend(init_times: list, **kwargs):
-    lines = [Line2D([0], [0],
-                    color=REFDATE_COLORBAR_TP[i],
-                    linewidth=REFDATE_LINEWIDTH[i],
-                    linestyle='-') for i in range(len(init_times)-1, -1, -1)]
-    labels = [f'init: {init_time.strftime("%Y-%m-%d %H:%M")}UTC' for
-              init_time in init_times[::-1]]
+    lines = [
+        Line2D(
+            [0],
+            [0],
+            color=REFDATE_COLORBAR_TP[i],
+            linewidth=REFDATE_LINEWIDTH[i],
+            linestyle="-",
+        )
+        for i in range(len(init_times) - 1, -1, -1)
+    ]
+    labels = [
+        f'init: {init_time.strftime("%Y-%m-%d %H:%M")}UTC'
+        for init_time in init_times[::-1]
+    ]
     plt.legend(lines, labels, **kwargs, fontsize=12)
 
+
 if __name__ == "__main__":
-    briefing_time = pd.Timestamp("2024-07-31").tz_localize("UTC")
-    lead_hours_str = "12H"
-    figure = precip(briefing_time, lead_hours_str)
-    figure.savefig("test.png")
+    import intake
+
+    CATALOG_URL = "https://tcodata.mpimet.mpg.de/internal.yaml"
+    incatalog = intake.open_catalog(CATALOG_URL)
+    hifs = HifsForecasts(incatalog)
+    briefing_time1 = pd.Timestamp(2024, 8, 1).tz_localize("UTC")
+    current_time1 = pd.Timestamp(2024, 8, 1, 11).tz_localize("UTC")
+
+    precip(briefing_time1, "003H", current_time1, hifs)

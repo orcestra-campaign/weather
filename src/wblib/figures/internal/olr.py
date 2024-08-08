be8de2f979cd70b5f2a6c7fb11eda42bbf82bd00
Sebastian Ortega <sebastian.ortega@mpimet.mpg.de>
2024-08-08 10:27:32 +0200

    olr.py refactor

--- src/wblib/figures/internal/olr.py
+++ src/wblib/figures/internal/olr.py
@@ -11,10 +11,9 @@ import matplotlib.colors as mcolors
 import seaborn as sns
 import easygems.healpix as egh
 
-from wblib.figures.briefing_info import get_latest_forecast_issue_time
+from wblib.figures.briefing_info import get_valid_time
+from wblib.figures.hifs import HifsForecasts
 
-
-CATALOG_URL = "https://tcodata.mpimet.mpg.de/internal.yaml"
 CATALOG_OLR_CODE = "ttr"
 CATALOG_ICWV_CODE = "tcwv"
 
@@ -29,76 +28,47 @@ FIGURE_BOUNDARIES = (-70, 10, -10, 30)
 
 
 def toa_outgoing_longwave(
-    briefing_time: pd.Timestamp, lead_hours: str, *args, **kwargs
+    briefing_time: pd.Timestamp,
+    lead_hours: str,
+    current_time: pd.Timestamp,
+    hifs: HifsForecasts,
 ) -> Figure:
-    lead_delta = pd.Timedelta(hours=int(lead_hours[:-1]))
-    issued_time = get_latest_forecast_issue_time(briefing_time)
-    valid_time = briefing_time + lead_delta
-    catalog = intake.open_catalog(CATALOG_URL)
-    accumulated_ttr = _get_forecast_datarray(
-        catalog, issued_time, CATALOG_OLR_CODE
+    issue_time, diff_ttr = hifs.get_forecast(
+        CATALOG_OLR_CODE,
+        briefing_time,
+        lead_hours,
+        current_time,
+        differentiate=True,
+        differentiate_unit="s",
     )
-    icwv = _get_forecast_datarray(catalog, issued_time, CATALOG_ICWV_CODE)
-    olr = _convert_accumulated_ttr_to_olr(accumulated_ttr, issued_time)
-    olr = olr.sel(time=valid_time.tz_localize(None))
-    icwv = icwv.sel(time=valid_time.tz_localize(None))
+    olr = - diff_ttr
+    _, icwv = hifs.get_forecast(
+        CATALOG_ICWV_CODE, briefing_time, lead_hours, current_time
+    )
     # plot
     sns.set_context("talk")
     fig, ax = plt.subplots(
         figsize=FIGURE_SIZE, subplot_kw={"projection": ccrs.PlateCarree()}
     )
+    _format_axes(briefing_time, lead_hours, issue_time, ax)
     _draw_olr(olr, fig, ax)
     _draw_icwv_contour(icwv, ax)
-    _format_axes(valid_time, issued_time, lead_delta, ax)
     matplotlib.rc_file_defaults()
     return fig
 
 
-def _get_forecast_datarray(
-    catalog, issued_time: pd.Timestamp, variable: str
-) -> xr.DataArray:
-    refdate = issued_time.strftime("%Y-%m-%d")
-    dataset = catalog.HIFS(refdate=refdate).to_dask().pipe(egh.attach_coords)
-    datarray = dataset[variable]
-    return datarray
-
-
-def _convert_accumulated_ttr_to_olr(
-    accumulated_ttr: xr.DataArray, issued_time: pd.Timestamp
-) -> xr.DataArray:
-    olr = xr.zeros_like(accumulated_ttr)
-    for i, time in enumerate(accumulated_ttr.time.values):
-        if i == 0:
-            previous_time = issued_time
-            timedelta = pd.Timedelta(time - issued_time.tz_localize(None))
-            ttr_0 = -accumulated_ttr.loc[dict(time=time)]
-            olr.loc[dict(time=time)] = ttr_0 / timedelta.total_seconds()
-            continue
-        previous_time = accumulated_ttr.time.values[i - 1]
-        timedelta = pd.Timedelta(time - previous_time)
-        ttr_2 = -accumulated_ttr.loc[dict(time=time)]
-        ttr_1 = -accumulated_ttr.loc[dict(time=time - timedelta)]
-        olr.loc[dict(time=time)] = (ttr_2 - ttr_1) / timedelta.total_seconds()
-    return olr
-
-
 def _draw_olr(olr, fig, ax) -> None:
-    sns.set_context("talk")
     # get_orl_colormap
-    gist_ncar = plt.cm.gist_ncar_r(np.linspace(0.0, 1, 128))
+    gist_ncar = plt.cm.Spectral(np.linspace(0.0, 1, 128))
     white = np.array([1.0, 1.0, 1.0, 1.0])
     colors = np.vstack((gist_ncar, white))
     colormap = mcolors.LinearSegmentedColormap.from_list("olr", colors)
     # draw plot
-    lon_min, lon_max, lat_min, lat_max = FIGURE_BOUNDARIES
-    ax.set_extent([lon_min, lon_max, lat_min, lat_max])
-    ax.coastlines(lw=0.8)
     im = egh.healpix_show(
         olr, method="linear", cmap=colormap, vmin=OLR_MIN, vmax=OLR_MAX, ax=ax
     )
     # format colorbar
     fig.colorbar(im, label="OLR $W/m^2$", shrink=0.7)
-    matplotlib.rc_file_defaults()
 
 
 def _draw_icwv_contour(icwv, ax):
@@ -111,16 +81,20 @@ def _draw_icwv_contour(icwv, ax):
         levels=[ICWV_ITCZ_THRESHOLD],
         colors=color,
         linewidths=2,
-        linestyles = 'dashed'
+        linestyles="dashed",
     )
-    format_func = lambda level : f"{int(level)} mm"
+    format_func = lambda level: f"{int(level)} mm"
     ax.clabel(hcs, hcs.levels, inline=True, fontsize=10, fmt=format_func)
 
-def _format_axes(valid_time, issued_time, lead_delta, ax):
+
+def _format_axes(briefing_time, lead_hours, issue_time, ax):
     lon_min, lon_max, lat_min, lat_max = FIGURE_BOUNDARIES
+    valid_time = get_valid_time(briefing_time, lead_hours)
+    ax.set_extent([lon_min, lon_max, lat_min, lat_max])
+    ax.coastlines(lw=0.8)
     title_str = (
         f"Valid time: {valid_time.strftime('%Y-%m-%d %H:%M')} \n"
-        f"Lead hours: {int(lead_delta.total_seconds() / 3600):03d}"
+        f"Lead hours: {lead_hours}"
     )
     ax.set_title(title_str)
     ax.coastlines(lw=1.0, color="k")
@@ -130,10 +104,22 @@ def _format_axes(valid_time, issued_time, lead_delta, ax):
     ax.set_xlabel("Longitude \N{DEGREE SIGN}E")
     ax.set_xlim([lon_min, lon_max])
     ax.set_ylim([lat_min, lat_max])
-    annotation = f"Latest ECMWF IFS forecast initialization: {issued_time.strftime('%Y-%m-%d %H:%M %Z')}"
+    annotation = f"Latest ECMWF IFS forecast initialization: {issue_time.strftime('%Y-%m-%d %H:%M %Z')}"
     ax.annotate(
         annotation,
         (-21.25, -9),
         fontsize=8,
         bbox=dict(facecolor="white", edgecolor="none", alpha=1),
     )
+
+
+if __name__ == "__main__":
+    import intake
+
+    CATALOG_URL = "https://tcodata.mpimet.mpg.de/internal.yaml"
+    incatalog = intake.open_catalog(CATALOG_URL)
+    hifs = HifsForecasts(incatalog)
+    briefing_time1 = pd.Timestamp(2024, 8, 1).tz_localize("UTC")
+    current_time1 = pd.Timestamp(2024, 8, 1, 11).tz_localize("UTC")
+
+    toa_outgoing_longwave(briefing_time1, "003H", current_time1, hifs)

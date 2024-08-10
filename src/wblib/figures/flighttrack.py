55d684dd102a4b891c558aa7fd9e7ba47f5f38c3
Henning Franke <henning.franke.182@gmail.com>
2024-08-10 16:19:54 -0100

    New interface to access Python flight information

---
+++ src/wblib/figures/flighttrack.py
@@ -0,0 +1,16 @@
+from wblib.flights._define_flights import FLIGHTS
+from orcestra.flightplan import path_preview
+
+def _get_python_flightdata(
+        flight_id: str):
+    flight_time, flight_track = FLIGHTS[flight_id]()
+    flight = {
+        'time': flight_time,
+        'track': flight_track,
+        }
+    return flight
+
+if __name__ == "__main__":
+    flight_id = 'HALO-20240811a'
+    flight = _get_python_flightdata(flight_id)
+    

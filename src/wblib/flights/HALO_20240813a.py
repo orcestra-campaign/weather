bdc4aaf23b5a2bc79810a6ae7431fb5474c6a8a0
Henning Franke <henning.franke.182@gmail.com>
2024-08-11 12:06:52 -0100

    Updated flightplan for HALO-20240813a

--- src/wblib/flights/HALO_20240813a.py
+++ src/wblib/flights/HALO_20240813a.py
@@ -4,35 +4,37 @@ from orcestra.flightplan import sal, bco, LatLon, IntoCircle
 def _flight_HALO_20240813a():
      flight_time = pd.Timestamp(2024, 8, 13, 12, 0, 0).tz_localize("UTC")
 
-     radius = 100e3
+     radius = 130e3
+     radius_atr = 70e3
+
      airport = sal
-     north = LatLon(lat=17.475, lon=-22.223, label='north')
-     far_north = LatLon(lat=21.354, lon=-21.441, label='far_north')
-     circle_north = LatLon(lat=14.000, lon=-22.910, label='circle_north')
-     circle_mid = LatLon(lat=11.250, lon=-23.480, label='circle_mid')
-     circle_south = LatLon(lat=8.500, lon=-23.970, label='circle_south')
-     south = LatLon(lat=2.000, lon=-25.174, label='south')
-     return_sal = LatLon(lat=14.888, lon=-22.712, label='return_sal')
+     north_ec = LatLon(lat=17.475000, lon=-22.222900, label='north_ec')
+     north_tp = LatLon(lat=21.350000, lon=-21.440600, label='north_tp')
+     south_ec = LatLon(lat=5.000000, lon=-24.702000, label='south_ec')
+     south_tp = LatLon(lat=5.000000, lon=-23.502000, label='south_tp')
+     circle_north = LatLon(lat=16.000000, lon=-21.278500, label='circle_north')
+     circle_center = LatLon(lat=11.751952, lon=-22.160695, label='circle_center')
+     circle_south = LatLon(lat=7.500000, lon=-23.016200, label='circle_south')
+
+     circle_atr = LatLon(lat=15.5, lon=-22.1, label='circle_atr')
 
      leg_south = [
           airport,
-          north,
-          far_north,
-          north,
-          circle_north,
-          circle_mid,
-          circle_south,
-          south
+          north_ec,
+          north_tp,
+          north_ec,
+          south_ec,
+          south_tp
      ]
 
      leg_circles = [
           IntoCircle(circle_south, radius, 360),
-          IntoCircle(circle_mid, radius, 360),
-          IntoCircle(circle_north, radius, 360),
+          IntoCircle(circle_center, radius, 360),
+          IntoCircle(circle_north, radius, 360, enter = 90),
+          IntoCircle(circle_atr, radius_atr, 360)
      ]
      
      leg_home = [
-          return_sal,
           airport
      ]
 

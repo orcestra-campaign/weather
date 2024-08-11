21a45045aef4e265969bc55f7d42c08aaccad28e
Henning Franke <henning.franke.182@gmail.com>
2024-08-11 11:46:34 -0100

    Add flight tracking to the other plots

--- src/wblib/figures/internal/olr.py
+++ src/wblib/figures/internal/olr.py
@@ -15,6 +15,7 @@ from wblib.figures.briefing_info import INTERNAL_FIGURE_SIZE
 from wblib.figures.briefing_info import format_internal_figure_axes
 from wblib.figures.hifs import HifsForecasts
 from wblib.figures.sattrack import plot_sattrack
+from wblib.flights.flighttrack import plot_python_flighttrack
 
 CATALOG_OLR_CODE = "ttr"
 CATALOG_ICWV_CODE = "tcwv"
@@ -31,6 +32,7 @@ def toa_outgoing_longwave(
     lead_hours: str,
     current_time: pd.Timestamp,
     sattracks_fc_time: pd.Timestamp,
+    flight: dict,
     hifs: HifsForecasts,
 ) -> Figure:
     issue_time, diff_ttr = hifs.get_forecast(
@@ -57,6 +59,8 @@ def toa_outgoing_longwave(
     _draw_icwv_contour(icwv, ax)
     plot_sattrack(ax, briefing_time, lead_hours, sattracks_fc_time,
                   which_orbit="descending")
+    plot_python_flighttrack(flight, briefing_time, lead_hours, ax, color="C1",
+                            show_waypoints=False)
     matplotlib.rc_file_defaults()
     return fig
 
@@ -92,14 +96,15 @@ def _draw_icwv_contour(icwv, ax):
 
 if __name__ == "__main__":
     import intake
+    from wblib.flights.flighttrack import get_python_flightdata
 
     CATALOG_URL = "https://tcodata.mpimet.mpg.de/internal.yaml"
     incatalog = intake.open_catalog(CATALOG_URL)
     hifs = HifsForecasts(incatalog)
-    briefing_time1 = pd.Timestamp(2024, 8, 9).tz_localize("UTC")
-    current_time1 = pd.Timestamp(2024, 8, 9, 12).tz_localize("UTC")
+    briefing_time1 = pd.Timestamp(2024, 8, 11).tz_localize("UTC")
+    current_time1 = pd.Timestamp(2024, 8, 11, 12).tz_localize("UTC")
     sattracks_fc_time1 = pd.Timestamp(2024, 8, 5).tz_localize("UTC")
-
-    fig = toa_outgoing_longwave(briefing_time1, "108H", current_time1,
-                                sattracks_fc_time1, hifs)
+    test_flight = get_python_flightdata('HALO-20240813a')
+    fig = toa_outgoing_longwave(briefing_time1, "60H", current_time1,
+                                sattracks_fc_time1, test_flight, hifs)
     fig.savefig("test1.png")

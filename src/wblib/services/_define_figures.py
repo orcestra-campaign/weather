8a0d7168ba3246c20e3be8cb1806b45ef62d5646
Henning Franke <henning.franke.182@gmail.com>
2024-08-12 17:06:14 -0100

    Change infrastructure to create goes2go images

--- src/wblib/services/_define_figures.py
+++ src/wblib/services/_define_figures.py
@@ -1,7 +1,8 @@
 """Define the figures and the functions that generate them."""
 
 from wblib.figures.external.goes import current_satellite_image_ir
-from wblib.figures.external.goes import current_satellite_image_vis
+from wblib.figures.external.goes_2_go import latest_goes2go_image
+from wblib.figures.external.goes_2_go import yesterdays_goes2go_image
 from wblib.figures.external.noaa import nhc_hovmoller
 from wblib.figures.external.noaa import nhc_seven_days_outlook
 from wblib.figures.external.noaa import nhc_surface_analysis_atlantic
@@ -10,9 +11,13 @@ from wblib.figures.external.ecmwf import ifs_dust
 from wblib.figures.internal.icwv import iwv_itcz_edges
 from wblib.figures.internal.sfc_winds import sfc_winds
 from wblib.figures.internal.precip import precip
-from wblib.figures.internal.olr import toa_outgoing_longwave
 from wblib.figures.internal.cld_top_height import cloud_top_height
 
+
+GOES2GO_PLOTS = {
+    "latest_goes2go_image": latest_goes2go_image,
+    "yesterdays_goes2go_image": yesterdays_goes2go_image,
+}
 
 EXTERNAL_INST_PLOTS = {
     "nhc_surface_analysis_atlantic": nhc_surface_analysis_atlantic,

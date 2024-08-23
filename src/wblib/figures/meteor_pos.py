11430458afbbe7c89e6bda46b995e92010b65157
Henning Franke <henning.franke.182@gmail.com>
2024-08-23 12:24:43 -0100

    Refactor Meteor track access, so that it's only conditionally accessed. Old functionality shouldn't change

--- src/wblib/figures/meteor_pos.py
+++ src/wblib/figures/meteor_pos.py
@@ -1,27 +1,35 @@
+import xarray as xr
 import pandas as pd
 from orcestra.meteor import get_meteor_track
 from wblib.figures.hifs import get_valid_time
 
 
 def plot_meteor_latest_position_in_ifs_forecast(
-        briefing_time: pd.Timestamp, lead_hours: str, ax, **kwargs):
+        briefing_time: pd.Timestamp, lead_hours: str, ax,
+        meteor: xr.Dataset=None, **kwargs):
     valid_time = get_valid_time(briefing_time, lead_hours)
     if briefing_time.date() == valid_time.date():
-        plot_meteor_latest_position(ax, **kwargs)
+        plot_meteor_latest_position(ax, meteor, **kwargs)
 
 
-def plot_meteor_latest_position(ax, **kwargs):
-    meteor = get_meteor_track(deduplicate_latlon=True)
+def plot_meteor_latest_position(
+        ax, meteor: xr.Dataset=None, **kwargs
+        ):
+    if not meteor: meteor = get_meteor_track(deduplicate_latlon=True)
     meteor_latest = meteor.isel(time=-1)
     ax.scatter(meteor_latest["lon"], meteor_latest["lat"], **kwargs)
 
 
-def plot_meteor_position(time: pd.Timestamp, ax, **kwargs):
-    meteor = get_meteor_track(deduplicate_latlon=True)
+def plot_meteor_position(
+        time: pd.Timestamp, ax, meteor: xr.Dataset=None, **kwargs
+        ):
+    if not meteor: meteor = get_meteor_track(deduplicate_latlon=True)
     meteor_pos = meteor.sel(time=time, method="nearest")
     ax.scatter(meteor_pos["lon"], meteor_pos["lat"], **kwargs)
 
 
-def plot_meteor_track(ax, **kwargs):
-    meteor = get_meteor_track(deduplicate_latlon=True)
+def plot_meteor_track(
+        ax, meteor: xr.Dataset=None, **kwargs
+        ):
+    if not meteor: meteor = get_meteor_track(deduplicate_latlon=True)
     ax.plot(meteor["lon"], meteor["lat"], **kwargs)

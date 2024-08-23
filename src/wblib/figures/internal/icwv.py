81736c68ce35ec88b3881ee88c162aee54ba52af
Henning Franke <henning.franke.182@gmail.com>
2024-08-23 12:00:11 -0100

    Add and test flightplan for HALO-20240825a

--- src/wblib/figures/internal/icwv.py
+++ src/wblib/figures/internal/icwv.py
@@ -104,9 +104,9 @@ if __name__ == "__main__":
     CATALOG_URL = "https://tcodata.mpimet.mpg.de/internal.yaml"
     incatalog = intake.open_catalog(CATALOG_URL)
     hifs = HifsForecasts(incatalog)
-    briefing_time1 = pd.Timestamp(2024, 8, 22).tz_localize("UTC")
-    current_time1 = pd.Timestamp(2024, 8, 22, 11).tz_localize("UTC")
+    briefing_time1 = pd.Timestamp(2024, 8, 24).tz_localize("UTC")
+    current_time1 = pd.Timestamp(2024, 8, 24, 11).tz_localize("UTC")
     sattracks_fc_time1 = pd.Timestamp(2024, 8, 21).tz_localize("UTC")
-    fig = iwv_itcz_edges(briefing_time1, "12H", current_time1,
+    fig = iwv_itcz_edges(briefing_time1, "36H", current_time1,
                          sattracks_fc_time1, hifs)
     fig.savefig("test2.png")

200a0d72c1f8c2fe559c5742abedaf6c132ee7ce
Henning Franke <henning.franke.182@gmail.com>
2024-08-31 15:54:56 -0100

    New interface for accessing climatology plus legend

--- src/wblib/figures/internal/icwv_ens.py
+++ src/wblib/figures/internal/icwv_ens.py
@@ -11,11 +11,12 @@ import warnings
 
 import matplotlib
 from matplotlib.figure import Figure
+from matplotlib.lines import Line2D
 import seaborn as sns
 
 from wblib.figures.briefing_info import INTERNAL_FIGURE_SIZE
 from wblib.figures.briefing_info import format_internal_figure_axes
-from wblib.figures.hifs import HifsForecasts
+from wblib.figures.hifs import HifsForecasts, get_valid_time
 from wblib.figures.sattrack import plot_sattrack
 from wblib.flights.flighttrack import plot_python_flighttrack
 from wblib.flights.flighttrack import get_python_flightdata
@@ -27,17 +28,8 @@ ICWV_MAX = 70  # mm
 ICWV_MIN = 45  # mm
 ICWV_COLORMAP = "Blues"
 ICWV_CATALOG_VARIABLE = "tcwv"
-REFDATE_COLORBAR = [
-    "#ff0000",
-    "#bf0000",
-    "#800000",
-    "#400000",
-    "#000000"
-] # the ordering of the colors indicate the latest available refdate
-REFDATE_LINEWIDTH = [1, 1.1, 1.2, 1.3, 1.5]
 
-
-def iwv_itcz_edges_ens(
+def iwv_itcz_edges_enfo(
     briefing_time: pd.Timestamp,
     lead_hours: str,
     current_time: pd.Timestamp,
@@ -54,6 +46,9 @@ def iwv_itcz_edges_ens(
         forecast_type="enfo"
     )
     _validate_issue_times(issue_time_oper, issue_time_enfo)
+    climatology = _get_rolling_daily_climatology(
+        ICWV_CATALOG_VARIABLE, briefing_time, lead_hours
+        )
     # plot
     sns.set_context("talk")
     fig, ax = plt.subplots(
@@ -61,10 +56,11 @@ def iwv_itcz_edges_ens(
         subplot_kw={"projection": ccrs.PlateCarree()}
     )
     format_internal_figure_axes(briefing_time, lead_hours, issue_time_oper,
-                                sattracks_fc_time, ax)
-    _draw_icwv_enfo_contours(forecast_enfo, ax)
-    _draw_icwv_enfo_mean_contour(forecast_enfo, ax)
-    _draw_icwv_oper_contour(forecast_oper, ax)
+                                sattracks_fc_time, "iwv_itcz_edges_enfo", ax)
+    plot1 = _draw_icwv_enfo_contours(forecast_enfo, ax)
+    plot2 = _draw_icwv_enfo_mean_contour(forecast_enfo, ax)
+    plot3 = _draw_icwv_oper_contour(forecast_oper, ax)
+    plot4 = _draw_icwv_ERA5_contour(climatology, ax)
     plot_sattrack(ax, briefing_time, lead_hours, sattracks_fc_time,
                   which_orbit="descending")
     for flight_id in FLIGHTS:
@@ -73,12 +69,15 @@ def iwv_itcz_edges_ens(
                                 color="C1", show_waypoints=False)
     plot_meteor_latest_position_in_ifs_forecast(
         briefing_time, lead_hours, ax, meteor=meteor_track)
+    
+    handles = [plot1, plot2, plot3, plot4]
+    plt.legend(handles=handles, fontsize=8, loc="lower left", frameon=False)
     matplotlib.rc_file_defaults()
     return fig
 
 
 def _validate_issue_times(
-        issue_time_oper: pd.Timestamp, issue_time_enfo):
+        issue_time_oper: pd.Timestamp, issue_time_enfo: pd.Timestamp):
     """Give a warning if issue times of operational forecast and ensemble
     forecast disagree."""
     msg = (f"WARNING: Issue times of operational forecast and ensemble " +
@@ -100,7 +99,9 @@ def _draw_icwv_oper_contour(forecast, ax):
         colors=color,
         linewidths=linewidth,
     )
-
+    line = Line2D([0], [0], label="Operational forecast", color=color,
+                  linewidth=linewidth)
+    return line
 
 def _draw_icwv_enfo_contours(forecasts, ax):
     color = "#bf0000"
@@ -115,11 +116,14 @@ def _draw_icwv_enfo_contours(forecasts, ax):
             linewidths=linewidth,
             alpha=alpha,
         )
+    line = Line2D([0], [0], label="Individual ensemble members", color=color,
+                  linewidth=linewidth, alpha=alpha)
+    return line
 
 
 def _draw_icwv_enfo_mean_contour(forecasts, ax):
-    color = "#800000",
-    linewidth = 2.0,
+    color = "#800000"
+    linewidth = 2.0
     enfo_mean = forecasts.mean(dim="member")
     egh.healpix_contour(
         enfo_mean,
@@ -128,6 +132,48 @@ def _draw_icwv_enfo_mean_contour(forecasts, ax):
         colors=color,
         linewidths=linewidth,
     )
+    line = Line2D([0], [0], label="Ensemble mean", color=color,
+                  linewidth=linewidth)
+    return line
+
+
+def _draw_icwv_ERA5_contour(climatology, ax):
+    color = "gray"
+    linewidth = 2.0
+    ls = '--'
+    #cat = intake.open_catalog("https://tcodata.mpimet.mpg.de/internal.yaml")
+    #hera5 = cat.HERA5.to_dask().pipe(egh.attach_coords)["tcwv"]
+    #augsep_clim = hera5.sel(
+    #    time=hera5['time'].dt.month.isin([8, 9])).mean(dim="time")
+    egh.healpix_contour(
+        climatology,
+        ax=ax,
+        levels=[ICWV_ITCZ_THRESHOLD],
+        colors=color,
+        linewidths=linewidth,
+        linestyles=ls,
+    )
+    line = Line2D([0], [0], label="ERA5 2010-2022 7-day running mean",
+                  color=color, linewidth=linewidth)
+    return line
+
+def _get_rolling_daily_climatology(
+        var: str,
+        briefing_time: pd.Timestamp,
+        lead_hours: str):
+    valid_time = get_valid_time(briefing_time, lead_hours)
+    valid_time_doy = valid_time.day_of_year
+    climatology = _load_rolling_daily_climatology(var)
+    return climatology.sel(dayofyear=valid_time_doy)
+
+def _load_rolling_daily_climatology(var: str="tcwv"):
+    cat = intake.open_catalog("https://tcodata.mpimet.mpg.de/internal.yaml")
+    hera5 = cat.HERA5(time="P1D").to_dask().pipe(egh.attach_coords)[var]
+    hera5_subsample = hera5.where(
+        hera5["time"] < np.datetime64("2023-01-01"), drop=True
+        )
+    hera5_running_mean = hera5_subsample.rolling(time=7, center=True).mean()
+    return hera5_running_mean.groupby('time.dayofyear').mean()
 
 
 if __name__ == "__main__":
@@ -141,6 +187,6 @@ if __name__ == "__main__":
     current_time1 = pd.Timestamp(2024, 8, 31, 10).tz_localize("UTC")
     sattracks_fc_time1 = pd.Timestamp(2024, 8, 31).tz_localize("UTC")
     meteor_track = get_meteor_track(deduplicate_latlon=True)
-    fig = iwv_itcz_edges_ens(briefing_time1, "12H", current_time1,
-                         sattracks_fc_time1, meteor_track, hifs)
+    fig = iwv_itcz_edges_enfo(briefing_time1, "12H", current_time1,
+                              sattracks_fc_time1, meteor_track, hifs)
     fig.savefig("test_icwv_enfo.png")

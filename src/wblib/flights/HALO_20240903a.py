a095b26bcb088f0c3812e0e56544ee2358d063c1
Henning Franke <henning.franke.182@gmail.com>
2024-09-03 12:00:59 -0100

    Updated flight HALO-20240903a and hardcoded its coordinates

--- src/wblib/flights/HALO_20240903a.py
+++ src/wblib/flights/HALO_20240903a.py
@@ -1,99 +1,51 @@
 import pandas as pd
-import numpy as np
-import orcestra.sat
-import orcestra.flightplan as fp
-from orcestra.flightplan import sal, mindelo, LatLon, IntoCircle, find_ec_lon
-
-def ec_time_at_lat(ec_track, lat):
-    e = np.datetime64("2024-08-01")
-    s = np.timedelta64(1, "ns")
-    return (((ec_track.swap_dims({"time":"lat"}).time - e) / s).interp(lat=lat) * s + e)
-
+import datetime
+from orcestra.flightplan import sal, LatLon, IntoCircle, FlightPlan
 
 def _flight_HALO_20240903a():
     flight_time = pd.Timestamp(2024, 9, 3, 12, 0, 0).tz_localize("UTC")
 
     radius = 72e3*1.852
     atr_radius = 38e3*1.852
+    airport = sal
 
-    band = "east"
-    airport = sal if band == "east" else bco
+    ec_north = LatLon(lat=14.75, lon=-29.64460064814815, label=None, fl=None, time=None, note=None)
+    ec_south = LatLon(lat=2.0, lon=-32.05971354102406, label=None, fl=None, time=None, note=None)
+    c_north  = LatLon(lat=13.0, lon=-29.983627306386914, label=None, fl=None, time=None, note=None)
+    c_south  = LatLon(lat=4.0, lon=-31.686613475177307, label=None, fl=None, time=None, note=None)
+    c_mid    = LatLon(lat=8.501448886319212, lon=-30.841571222673966, label=None, fl=None, time=None, note=None)
+    c_atr    = LatLon(ec_north.lat+atr_radius/1852./60.,-22.1, label = "c_atr")
+    j_beg    = LatLon(lat=ec_north.lat,lon=-25.).assign(label = "junction")
+    ec_under = LatLon(lat=10.301303243452365, lon=-30.50034768724039, label=None, fl=None, time=datetime.datetime(2024, 9, 3, 16, 8, 30, 758740, tzinfo=datetime.timezone.utc), note=None)
 
-    # Load ec satellite track for 
-    ec_track = orcestra.sat.SattrackLoader("EARTHCARE", "2024-08-29", kind="PRE").get_track_for_day(f"{flight_time:%Y-%m-%d}")
-    ec_track = ec_track.sel(time=slice(f"{flight_time:%Y-%m-%d} 14:00", None))
-    ec_lons, ec_lats = ec_track.lon.values, ec_track.lat.values
+    # Additional Waypoints
+    x_cnn = LatLon(lat=13.67, lon=-29.85425074074074, label=None, fl=None, time=None, note=None)
+    x_cns = LatLon(lat=11.5, lon=-30.271701820425793, label=None, fl=None, time=None, note=None)
+    x_cms = LatLon(lat=7.75, lon=-30.983356940160395, label=None, fl=None, time=None, note=None)
+    x_xsd = c_south.towards(c_mid).assign(label="x_sonde", note="Position for an extra sonde heading south")
+    j_atr = c_atr.towards(sal,distance=-atr_radius).assign(label="j_atr", note = "ATR circle start and end")
+    extra_waypoints = [LatLon(17.5,-24.00).assign(label="x_atr", note="alternative ATR circle")]
 
-    # Latitudes where we enter and leave the ec track (visually estimated)
-    lat_ec_north_in = 14
-    lat_ec_south = 2.0
-
-    # latitude of circle centers
-    lat_c_north = 11.5
-    lat_c_south = 4.0
-    lat_c_south_n = lat_c_south + 1.0
-    lat_c_south_s = lat_c_south - 1.0
-
-    lat_c_mid = lat_c_south + (lat_c_north-lat_c_south)/2.0
-    lat_c_mid_n = lat_c_mid + 1.0
-    lat_c_mid_s = lat_c_mid - 1.0
-
-    lat_ec_under = lat_c_north - (radius/60./1852. * np.cos(np.deg2rad(lat_c_north)))
-
-    c_atr_nw = LatLon(17.433,-23.500, label = "c_atr")
-    c_atr_se = LatLon(16.080,-21.715, label = "c_atr")
-
-    # create ec track
-    ec_north = LatLon(lat_ec_north_in, find_ec_lon(lat_ec_north_in, ec_lons, ec_lats), label = "ec_north")
-    ec_south = LatLon(lat_ec_south, find_ec_lon(lat_ec_south, ec_lons, ec_lats), label = "ec_south")
-
-    # create circles
-    c_north = LatLon(lat_c_north, find_ec_lon(lat_c_north, ec_lons, ec_lats), label = "c_north")
-
-    c_south = LatLon(lat_c_south, find_ec_lon(lat_c_south, ec_lons, ec_lats), label = "c_south")
-    c_south_s = LatLon(lat_c_south_s, find_ec_lon(lat_c_south_s, ec_lons, ec_lats), label = "c_south_s")
-    c_south_n = LatLon(lat_c_south_n, find_ec_lon(lat_c_south_n, ec_lons, ec_lats), label = "c_south_n")
-
-    c_mid = LatLon(lat_c_mid, find_ec_lon(lat_c_mid, ec_lons, ec_lats), label = "c_mid")
-    c_mid_s = LatLon(lat_c_mid_s, find_ec_lon(lat_c_mid_s, ec_lons, ec_lats), label = "c_mid_s")
-    c_mid_n = LatLon(lat_c_mid_n, find_ec_lon(lat_c_mid_n, ec_lons, ec_lats), label = "c_mid_n")
-
-    c_atr = c_atr_se
-
-    # ec underpass
-    ec_under = LatLon(lat_ec_under, find_ec_lon(lat_ec_under, ec_lons, ec_lats), label = "ec_under")
-    ec_under = ec_under.assign(time=fp.ec_time_at_lat(ec_track, float(ec_under.lat)))
-
-    # Define flight track
-    outbound_legs = [
+    # Define Flight Paths
+    waypoints = [
         airport.assign(fl=0),
+        j_beg.assign(fl=390),
         ec_north.assign(fl=410),
-        ec_south.assign(fl=410),
-        ]
-
-    ec_legs = [
-        IntoCircle(c_south.assign(fl=410), radius, -360),   
-        IntoCircle(c_mid.assign(fl=430), radius, -360), 
-        c_mid.assign(fl=430),
+        c_mid.assign(fl=410),
+        x_xsd.assign(fl=410),
+        IntoCircle(c_south.assign(fl=410), radius, -360),
+        x_cms.assign(fl=450).assign(note='might be to soon to rise to FL450, if so rise to FL430 and higher later'),
+        IntoCircle(c_mid.assign(fl=450), radius, -360),
         ec_under.assign(fl=450),
-        c_north.assign(fl=450),
-        IntoCircle(c_north.assign(fl=450), radius, -360,enter=98),   
+        x_cns.assign(fl=450),
+        IntoCircle(c_north.assign(fl=450), radius, -360,enter=90),
+        x_cnn.assign(fl=450),
         ec_north.assign(fl=450),
-        ]
-    inbound_legs = [
-        airport.assign(fl=350),
-        IntoCircle(c_atr.assign(fl=350), atr_radius, -360), 
+        j_beg.assign(fl=450),
+        j_atr.assign(fl=350),
+        IntoCircle(c_atr.assign(fl=350), atr_radius,-360),
         airport.assign(fl=0),
         ]
 
-    waypoints = outbound_legs + ec_legs + inbound_legs 
-
-    waypoint_centers = []
-    for point in waypoints:
-        if isinstance(point, IntoCircle):
-            point = point.center
-        waypoint_centers.append(point)
-
-    path = fp.expand_path(waypoints, dx=10e3)
-
-    return flight_time, path
+    plan = FlightPlan(waypoints, None, extra_waypoints=extra_waypoints)
+    return flight_time, plan.path
